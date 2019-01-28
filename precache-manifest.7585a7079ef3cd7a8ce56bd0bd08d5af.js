4a7134adc260e7736ad6b77770c8f5d0f218f529
Suraj Sharma <www.surajsharma@gmail.com>
2019-01-28 20:19:48 +0530

    Updates

---
+++ precache-manifest.7585a7079ef3cd7a8ce56bd0bd08d5af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea91fca009f17fbde6f9",
+    "url": "/reactodo/static/css/main.f05f3860.chunk.css"
+  },
+  {
+    "revision": "ea91fca009f17fbde6f9",
+    "url": "/reactodo/static/js/main.ea91fca0.chunk.js"
+  },
+  {
+    "revision": "3c0e9fff00d6b015082a",
+    "url": "/reactodo/static/js/1.3c0e9fff.chunk.js"
+  },
+  {
+    "revision": "e1e9d21f5303dfb5b8b1",
+    "url": "/reactodo/static/js/runtime~main.e1e9d21f.js"
+  },
+  {
+    "revision": "c147d9f952cc975f156a0f686791996f",
+    "url": "/reactodo/index.html"
+  }
+];
